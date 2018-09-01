b1ab9c54f7bb2a6ef495db64aace2e0b86107d32
thishome153 <home153@mail.ru>
2018-09-02 00:00:07 +0300

    -

---
+++ XMLReaderCS/frmPointEditor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XMLReaderCS
+{
+    public partial class frmPointEditor : Form
+    {
+        public frmPointEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
